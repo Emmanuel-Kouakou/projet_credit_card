5d71ff9ab2f86b149150df3ed66607ecc95f4f8a
Emmanuel-Kouakou <82089534+Emmanuel-Kouakou@users.noreply.github.com>
2021-08-02 16:12:00 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[theme]
+primaryColor = "#E694FF"
+backgroundColor = "#00172B"
+secondaryBackgroundColor = "#0083B8"
+textColor = "#DCDCDC"
+font = "sans-serif"
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS=false\n\
+\n\
+" > ~/.streamlit/config.toml
